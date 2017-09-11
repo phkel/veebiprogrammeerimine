b000a9c0c3b6a4b00c589a87d51d36f96cbadd78
Kertu Kipper <phkel@users.noreply.github.com>
2017-09-11 10:37:30 +0300

    HTML koodile lisatud esimene PHP osa

---
+++ esimene.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Kertu Kipper</title>
+    <link rel="stylesheet" href="main.css">
+  </head>
+  <body>
+      <div class="container">
+        <h1>Veebiprogrammeerimine</h1>
+        <p>Veebileht on loodud veebiprogrammeerimise kursusel ning ei sisalda tõsiseltvõetavat sisu.</p>
+        <h2>Unde impedit</h2>
+        <p>Consequatur voluptatibus eligendi architecto. Odio dolores corrupti error. Vel amet quas vel totam repudiandae. Ducimus necessitatibus et repellat corporis eveniet doloremque tenetur.
+        </p>
+        <p>
+        Et molestiae accusantium aut impedit et nam cumque non. Temporibus enim veniam quo. Iste qui vel tenetur sed optio placeat officia officia. Natus sapiente dolorem perferendis repellendus. Voluptatem cupiditate perferendis eum sed recusandae doloremque est. Nihil vel dolores temporibus voluptas est veniam fuga consequuntur.
+        </p>
+        <p>
+        Repudiandae harum incidunt optio quod provident cumque qui. Ut quae quibusdam omnis. Aperiam maiores in ab. Rem sequi et qui veniam laudantium.
+        </p>
+        <?php
+          echo "Täna on ilus ilm.";
+        ?>
+      </div>
+  </body>
+</html>
